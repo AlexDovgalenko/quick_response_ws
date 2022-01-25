ee05209a1939b8ac1f443a2666050adf88b02930
Oleksandr Dovgalenko <Oleksandr.Dovgalenko@harmonicinc.com>
2022-01-26 00:41:48 +0200

    refactor

--- access_code_generator.py
+++ access_code_generator.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
@@ -13,6 +15,7 @@ def generate_access_code():
         return render_template("pass_form.html")
     return render_template("generator_3.html")
 
+
 @app.route("/validate_access_code", methods=["POST"])
 def validate_pass_code():
     pass_code = request.form["pass-code"]
@@ -21,6 +24,15 @@ def validate_pass_code():
         is_valid_code = True
         return redirect(url_for("generate_access_code"))
     return render_template("pass_form.html")
+
+
+@app.route("/generate_response/", methods=["GET"])
+def generate_response():
+    digit = randint(100, 200)
+    text_response = f"Случайное число в диапазоне от 100 до 200: {digit}"
+    # return jsonify(str=text_response)
+    # return render_template("generator.html", digit=digit)
+    return render_template("generator_2.html", digit=digit)
 
 
 if __name__ == "__main__":
