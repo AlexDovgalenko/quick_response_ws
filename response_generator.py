1eedad579e41bbf6a8f5ea19d115be3376c5ae84
Alexander Dovgalenko <a.dovgalenko2@gmail.com>
2022-01-25 10:27:51 +0200

    Added countdown (#4)
    * added second generator template and countdown template
    
    * updated generator
    
    * updated generator_2
    
    * updated countdown
    
    * response_generator

--- response_generator.py
+++ response_generator.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def generate_response():
     digit = randint(100, 200)
     text_response = f"Случайное число в диапазоне от 100 до 200: {digit}"
-    # text_response = f"Random digit in range from 100 to 200: {digit}"
     # return jsonify(str=text_response)
-    return render_template("generator.html", digit=digit)
+    # return render_template("generator.html", digit=digit)
+    return render_template("generator_2.html", digit=digit)
 
 
 if __name__ == "__main__":
