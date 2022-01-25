d38a4b8ef86bba0af4a590d5f40a43b0049f226f
Oleksandr Dovgalenko <Oleksandr.Dovgalenko@harmonicinc.com>
2022-01-26 00:36:32 +0200

    added form

---
+++ access_code_generator.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+is_valid_code = False
+timer = None
+PASS_CODE = "QX4ED7"
+
+
+@app.route("/access_code_gen", methods=["GET"])
+def generate_access_code():
+    if not is_valid_code:
+        return render_template("pass_form.html")
+    return render_template("generator_3.html")
+
+@app.route("/validate_access_code", methods=["POST"])
+def validate_pass_code():
+    pass_code = request.form["pass-code"]
+    global is_valid_code
+    if pass_code == PASS_CODE:
+        is_valid_code = True
+        return redirect(url_for("generate_access_code"))
+    return render_template("pass_form.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
