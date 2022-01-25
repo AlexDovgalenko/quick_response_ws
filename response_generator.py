9aaca1bd513a5e722c2454ef55b7d9b1eeedebb3
AlexDovgalenko <a.dovgalenko2@gmail.com>
2022-01-25 09:36:52 +0200

    added second generator template and countdown template

--- response_generator.py
+++ response_generator.py
@@ -11,7 +11,8 @@ def generate_response():
     text_response = f"Случайное число в диапазоне от 100 до 200: {digit}"
     # text_response = f"Random digit in range from 100 to 200: {digit}"
     # return jsonify(str=text_response)
-    return render_template("generator.html", digit=digit)
+    # return render_template("generator.html", digit=digit)
+    return render_template("generator_2.html", digit=digit)
 
 
 if __name__ == "__main__":
